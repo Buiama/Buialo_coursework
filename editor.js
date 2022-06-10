d970c18eb191622850cd4418b4767c0af54cf6ca
Buiama <83916242+Buiama@users.noreply.github.com>
2022-06-10 16:36:26 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -7,3 +7,30 @@ editor.setOptions({
   fontSize:"12pt",
 });
 editor.$blockScrolling=Infinity;
+
+
+function run() {
+  var i = editor.getValue();
+  // Команда для компиляции на удаленном сервере
+  let cmd = "g++ -Wall main.cpp -o main_prog && echo 'Compilation: SUCCESS."
+      + "\n' && ./main_prog && echo \"\nExit code: $?\"";
+  var output = $("#output");
+  
+  output.text('Compiling...');
+  var to_compile = {
+    "src": i,
+    "cmd": cmd,
+  };
+  output.text("Executing... Please wait.");
+  $.ajax({
+    url: "http://coliru.stacked-crooked.com/compile",
+    type: "POST",
+    data: JSON.stringify(to_compile),
+    contentType:"text/plain; charset=utf-8",
+    dataType: "text"
+  }).done(function(data) {
+    output.text(data);
+  }).fail(function(data) {
+    output.text("Error: " + data);
+  });
+};
